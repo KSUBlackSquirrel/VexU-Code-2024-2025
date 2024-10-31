4907608239dae75e05d911ed03a58ce226ff850c
JacobPaulFord <the5linez@gmail.com>
2024-10-30 21:15:25 -0400

    Drive Train Test Created
    First test of drive train 1

--- DriveTrainTest/src/main.cpp
+++ DriveTrainTest/src/main.cpp
@@ -1,8 +1,8 @@
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /*    Module:       main.cpp                                                  */
-/*    Author:       Ian Laptop                                                */
-/*    Created:      10/16/2024, 8:45:01 PM                                    */
+/*    Author:       the5l                                                     */
+/*    Created:      10/30/2024, 8:13:44 PM                                    */
 /*    Description:  V5 project                                                */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
@@ -22,16 +22,49 @@ enum Cartridge {
   BLUE   // 600 rom
 };
 
-motor Motor1 = motor(PORT11);
-motor DriveMotor{Motor1};
-Cartridge Motor1Color = GREEN;
+motor RMotor1 = motor(PORT1);
+motor RMotor2 = motor(PORT10);
+
+motor_group RDriveTrain;
+
+motor LMotor1 = motor(PORT11);
+motor LMotor2 = motor(PORT20);
+
+motor_group LDriveTrain;
+
+Cartridge DriveTrainColor = GREEN;
+controller Controller = controller();
+
+int LAxis;
+int RAxis;
+
+
+
+
 brain Brain;
 
+
 void drive_config() {
-   Brain.Screen.print("color:" + (Motor1Color == RED) ? 100 : ((Motor1Color == GREEN) ? 200 : (Motor1Color == BLUE) ? 600 : 0)); Brain.Screen.newLine();
-  Motor1.setStopping(brake);
-  Motor1.setVelocity((Motor1Color == RED) ? 100 : ((Motor1Color == GREEN) ? 200 : (Motor1Color == BLUE) ? 600 : 0), rpm);
+  Brain.Screen.print("robot_config "); 
+  RMotor1.setReversed(true);
+  RMotor2.setReversed(true);
+
+  motor_group RDriveTrain = motor_group(RMotor1,RMotor2);
+
+  LMotor1.setReversed(false);
+  LMotor2.setReversed(false);
+
+  motor_group LDriveTrain = motor_group(LMotor1,LMotor2);
+
+
+  RDriveTrain.setStopping(brake);
+  LDriveTrain.setStopping(brake);
+
+  RDriveTrain.setVelocity((DriveTrainColor == RED) ? 100 : ((DriveTrainColor == GREEN) ? 200 : (DriveTrainColor == BLUE) ? 600 : 0), rpm);
+  LDriveTrain.setVelocity((DriveTrainColor == RED) ? 100 : ((DriveTrainColor == GREEN) ? 200 : (DriveTrainColor == BLUE) ? 600 : 0), rpm);
+  drivetrain Drivetrain = drivetrain(LDriveTrain, RDriveTrain, 259.34, 320, 40, mm, 1);
 }
+
 
 /*---------------------------------------------------------------------------*/
 /*                          Pre-Autonomous Functions                         */
@@ -44,9 +77,10 @@ void drive_config() {
 /*---------------------------------------------------------------------------*/
 
 void pre_auton(void) {
+
   // All activities that occur before the competition starts
   // Example: clearing encoders, setting servo positions, ...
-  Brain.Screen.print("pre_auton"); Brain.Screen.newLine();
+
   
   // Runs any motor configs necessary to drive
   drive_config();
@@ -66,19 +100,6 @@ void autonomous(void) {
   // ..........................................................................
   // Insert autonomous user code here.
   // ..........................................................................
-  Brain.Screen.print("autonomous ");
-  
-  Brain.Screen.print("forward ");
-  Motor1.spin(forward);
-  wait(4, sec);
-  Brain.Screen.print("break ");
-  Motor1.stop();
-  wait(2, sec);
-  Brain.Screen.print("reverse ");
-  Motor1.spin(reverse);
-  wait(5, sec);
-  Brain.Screen.print("break"); Brain.Screen.newLine();
-  Motor1.stop();
 }
 
 /*---------------------------------------------------------------------------*/
@@ -92,7 +113,6 @@ void autonomous(void) {
 /*---------------------------------------------------------------------------*/
 
 void usercontrol(void) {
-  Brain.Screen.print("usercontrol"); Brain.Screen.newLine();
   // User control code here, inside the loop
   while (1) {
     // This is the main execution loop for the user control program.
@@ -103,6 +123,29 @@ void usercontrol(void) {
     // Insert user code here. This is where you use the joystick values to
     // update your motors, etc.
     // ........................................................................
+    LAxis = Controller.Axis3.position();
+    RAxis = Controller.Axis2.position();
+    
+    Brain.Screen.print("LAxis:" + LAxis);
+    Brain.Screen.print("RAxis:" + RAxis); Brain.Screen.newLine();
+    
+
+    if (LAxis >= 10) {
+        LDriveTrain.spin(forward);
+    }else if (LAxis <= -10){
+        LDriveTrain.spin(reverse);
+    }else{
+      LDriveTrain.stop();
+    }
+
+    if (RAxis >= 10) {
+        RDriveTrain.spin(forward);
+    }else if (RAxis <= -10){
+        RDriveTrain.spin(reverse);
+    }else{
+      RDriveTrain.stop();
+    }
+
 
     wait(20, msec); // Sleep the task for a short amount of time to
                     // prevent wasted resources.
