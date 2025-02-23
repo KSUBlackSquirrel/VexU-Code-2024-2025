61fcf8ceb4f049557ac9059613d09b590b94d4e1
Ian Rohrbacher <ianjay1026@gmail.com>
2025-02-22 21:55:32 -0500

    the narnia update

--- Test2024/24in Auto tests/src/subsystem/drive.cpp
+++ Test2024/24in Auto tests/src/subsystem/drive.cpp
@@ -86,6 +86,8 @@ lemlib::Chassis chassis(
 
 
 void init() {
+    left_motor_group.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+    right_motor_group.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
     chassis.calibrate(); // calibrate sensors
 }
 
@@ -95,6 +97,8 @@ lemlib::Pose pos(){
 
 ASSET(BigBotStart1_txt);
 ASSET(BigBotStart2_txt);
+ASSET(BigBotStart3_txt);
+ASSET(BigBotStart4_txt);
 
 void autonomous(){
     // Tune Angular PID
@@ -110,27 +114,33 @@ void autonomous(){
 
 
 
-	chassis.setPose(-58.974, 0.492, 90); // starting pos
+	chassis.setPose(-63.5, 0, 90); // starting pos
     //
     //Start path (puts first ring on side pole) ----------*******Need to start bot with slight tilt to the right**********
     //
     intake::IntakeUp(600);
-    conveyor::conveyorUp(600);
-    chassis.follow(BigBotStart1_txt, 6, 1000);
-    conveyor::conveyorStop();
-    pros::delay(200); 
+    conveyor::conveyorUp(50);
+    chassis.follow(BigBotStart1_txt, 6, 1000, true);
+    pros::delay(1500); 
     chassis.follow(BigBotStart2_txt, 6, 1000, false);
-
-
-    left_motor_group.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
-    right_motor_group.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
-    pros::delay(900); 
-    conveyor::conveyorUp(600);
-    pros::delay(3000); 
-    // left_motor_group.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
-    // right_motor_group.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+    pros::delay(1000);   
+    chassis.turnToHeading(105, 500);
+    pros::delay(500);  
+    conveyor::conveyorUp(500);
+    pros::delay(1500);     
     conveyor::conveyorStop();
-
+    chassis.follow(BigBotStart3_txt, 6, 1000);
+    pros::delay(1000);
+    chassis.turnToHeading(157, 750);
+    pros::delay(750);
+	//chassis.setPose(-50, 0, chassis.getPose().theta);
+    //chassis.moveToPoint(-60, -100, 5000);
+    chassis.follow(BigBotStart4_txt, 6, 2500, true);
+    pros::delay(1000);
+    conveyor::conveyorUp(40);
+    pros::delay(4000);
+    // pros::delay(2500);
+ 
 
     //intake::IntakeStop();
     // conveyor::conveyorDown(500);
